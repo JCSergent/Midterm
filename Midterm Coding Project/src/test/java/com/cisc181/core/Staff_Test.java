ac1622085c291933198f8fe01bca38775bd439bd
jcser <jcser@JCs-Laptop>
2018-03-20 23:37:09 -0400

    Midterm

--- Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
@@ -3,10 +3,15 @@ package com.cisc181.core;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Date;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.cisc181.eNums.eTitle;
 
 public class Staff_Test {
 
@@ -16,7 +21,25 @@ public class Staff_Test {
 	
 	@Test
 	public void test() {
-		assertEquals(1,1);
-	}	
+
+		ArrayList<Staff> staff = new ArrayList<Staff>();
+		staff.add(new Staff("James", "Carroll", "Sergent", new Date(10,20,1999), "2134 Holly Springs", "9180381923", "hey@gmail.com", "9:00-10:00", 4, 150.0, new Date(10,10,2017), eTitle.MR));
+		staff.add(new Staff("James", "Carroll", "Sergent", new Date(10,20,1999), "2134 Holly Springs", "9180381923", "hey@gmail.com", "9:00-10:00", 4, 200.0, new Date(10,10,2017), eTitle.MR));
+		staff.add(new Staff("James", "Carroll", "Sergent", new Date(10,20,1999), "2134 Holly Springs", "9180381923", "hey@gmail.com", "9:00-10:00", 4, 100.0, new Date(10,10,2017), eTitle.MR));
+		staff.add(new Staff("James", "Carroll", "Sergent", new Date(10,20,1999), "2134 Holly Springs", "9180381923", "hey@gmail.com", "9:00-10:00", 4, 150.0, new Date(10,10,2017), eTitle.MR));
+		staff.add(new Staff("James", "Carroll", "Sergent", new Date(10,20,1999), "2134 Holly Springs", "9180381923", "hey@gmail.com", "9:00-10:00", 4, 100.0, new Date(10,10,2017), eTitle.MR));
+		double totalSalary=0;
+		for(Staff staf : staff ) {
+			totalSalary += staf.getSalary();
+		}
+		double average = totalSalary/5;
+		System.out.print(average);
+		assertEquals(average,140.0,0);
+	}
+	
+	@Test
+	public void test2() {
+		Staff person = new Staff("James", "Carroll", "Sergent", new Date(10,20,1999), "2134 Holly Springs", "9180381923", "hey@gmail.com", "9:00-10:00", 4, 150.0, new Date(10,10,2017), eTitle.MR);
+	}
 
 }
--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -4,14 +4,51 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.cisc181.eNums.eMajor;
 
 public class Student_Test {
 
 	@BeforeClass
 	public static void setup() {
+		ArrayList<Course> courses = new ArrayList<Course>();
+		courses.add(new Course("Econ",100,eMajor.BUSINESS));
+		courses.add(new Course("Java",100,eMajor.COMPSI));
+		courses.add(new Course("Physics",100,eMajor.PHYSICS));
+		
+		ArrayList<Semester> semesters = new ArrayList<Semester>();
+		semesters.add(new Semester(new Date(8,20,2017), new Date(12,25,2017)));
+		semesters.add(new Semester(new Date(2,5,2018), new Date(4,20,2018)));
+		
+		ArrayList<Section> sections = new ArrayList<Section>();
+		for(Semester s : semesters) {
+			for(Course c : courses) {
+				sections.add(new Section(s.getSemsterID(), c.getCourseID(), 10));
+			}
+		}
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+		students.add(new Student("James", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Hames", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Games", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Lames", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Fames", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Wames", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Mames", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Tames", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Cames", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		students.add(new Student("Bames", "Sergent", "Carroll",new Date(10,20,1999), eMajor.COMPSI, "2513 Holly", "4104104100", "jcsergent20@udel.edu"));
+		
+		ArrayList<Enrollment> enrollments = new ArrayList<Enrollment>();
+		for(Student s : students) {
+			for(Section a : sections) {
+				enrollments.add(new Enrollment(s.getStudentID(),a.getSectionID(), 80));
+			}
+		}
 	}
 
 	@Test
